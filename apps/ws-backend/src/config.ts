38fe95707c2f3d323bebe448a518a2fafc165486
AKS <sumananupkumar733@gmail.com>
2025-10-14 19:53:17 +0530

    ak updated

---
+++ apps/ws-backend/src/config.ts
@@ -0,0 +1,1 @@
+export const jwtsecret="12534";
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,9 +1,24 @@
 import { WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import {jwtsecret} from './config';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, request) {
   ws.on('error', console.error);
+
+  const url = request.url;
+  if(!url){
+    return
+  }
+  const queryParams = new URLSearchParams(url.split('?')[1]);
+  const token = queryParams.get('token')||"";
+
+  const decode = jwt.verify(token, jwtsecret);
+  if(!decode || !(decode as JwtPayload).userId){
+    wss.close();
+    return;
+  }
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
