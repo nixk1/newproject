a05d6910f22e130b79a358623a58532ca9f749d2
Nikhil kumar <168296421+nixk1@users.noreply.github.com>
2025-10-16 16:39:10 +0530

    tsconfig file changed

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,10 +1,10 @@
 import {Request, NextFunction,Response } from "express";
 import jwt from "jsonwebtoken"
-import { jwtsecreat } from "./config";
+import { jwtsecret } from "@repo/backend-common/config";
 export function middleware(req:Request,res:Response,next:NextFunction){
   const token=req.headers["authorization"] ?? "";
 
-  const decode=jwt.verify(token,jwtsecreat);
+  const decode=jwt.verify(token,jwtsecret);
 
   if(decode){
     //@ts-ignore: Todo: fix his??
